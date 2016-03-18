a05fe27012c81dabb1d82490666a3bfb8b1f0336
May <m544498510@gmail.com>
2016-03-18 17:34:49 +0800

    增加路由模块

--- webapp/src2/modules/managerApp/app/test.main.js
+++ webapp/src2/modules/managerApp/app/test.main.js
@@ -5,13 +5,40 @@ requirejs.config({
     baseUrl: './',
     paths: {
         'jquery':'dist/lib/jquery/dist/jquery.min',
-        'artTemplate':'dist/lib/artTemplate/dist/template'
+        'artTemplate':'dist/lib/artTemplate/dist/template',
+        'StateMan':'dist/lib/stateman/stateman.min'
     },
     shim: {
     }
 });
 
-define(["taskListViewModule","testController"],function (a,b) {
+define(['StateMan',"taskListViewModule","testController"],function (StateMan,a,b) {
     a.init();
     b.init();
+    var config = {
+        enter: function(){
+            console.log("enter: " + this.name)
+        },
+        leave: function(){
+            console.log("leave: " + this.name)
+        }
+    };
+
+    function create(o){
+        o = o || {};
+        o.enter= config.enter;
+        o.leave = config.leave;
+        return o;
+    }
+
+    var stateman = new StateMan();
+
+    stateman
+        .state("home", config)
+        .state("contact", config)
+        .state("contact.list", config )
+        .state("contact.detail", create({url: ":id(\\d+)"}))
+        .state("contact.detail.option", config)
+        .state("contact.detail.message", config)
+        .start({});
 });
