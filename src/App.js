0722e9b2462011a76382e351e5ab7eedce09de06
Xinlin Mei <xinlin.mei@ygomi.com>
2018-12-28 17:05:54 +0800

    fix bug

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {of} from 'rxjs';
+
+import './App.css';
 
 class App extends Component {
+  state = {
+    data: []
+  };
+  
+  componentDidMount() {
+    of('1', '2', '3')
+      .subscribe((v) => {
+        const data = this.state.data;
+        data.push(v);
+        this.setState({data:data});
+      });
+    
+  }
+  
   render() {
     return (
       <div className="App">
-        test 123
+        {
+          this.state.data.map(item => (
+            <div key={item}>{item}</div>
+          ))
+        }
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,14 +3,5 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import { of} from 'rxjs';
-
-of('1','2','3')
-  .subscribe((v) => {
-    console.log(v);
-  });
-
-
-
 ReactDOM.render(<App />, document.getElementById('root'));
 
